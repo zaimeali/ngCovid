0f1a757b1ba72cf31316a56d1793520c1580fd5f
Zaime Ali <zaimeali1997@gmail.com>
2020-06-08 02:53:37 +0500

    UI at Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppComponent},
+  { path: '', pathMatch: 'full', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
