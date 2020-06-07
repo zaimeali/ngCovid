d9537a0270e5d642e98509115ef1923f1b55434b
Zaime Ali <zaimeali1997@gmail.com>
2020-06-08 02:31:32 +0500

    Header Done Mostly

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # CoronaApiApp
 
+
+1) Sidebar: https://www.youtube.com/watch?v=5uC5vdjifE4
+2) Promises for loading
+3) Country in a Sidebar
+4) Content in a main
+
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.5.
 
 ## Development server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
